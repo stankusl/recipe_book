97adb47585395b22d5eead04ed9eabde78efaa9c
Luke Stankus <itrash.1990@gmail.com>
2016-11-28 22:40:14 +0000

    added node server configs

---
+++ node_server.js
@@ -0,0 +1,10 @@
+// Server Configuration for Node Server.
+var express = require('express');
+var modRewrite = require('connect-modrewrite');
+
+var server = express();
+server.use( modRewrite([
+      '!\\.\\w+$ /index.html [L]'
+]));
+server.use('/', express.static(__dirname + '/build'));
+server.listen(4420);
