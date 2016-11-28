9ee3b1491ab30ea672d831dcc327296b18883e74
Luke Stankus <itrash.1990@gmail.com>
2016-11-28 09:13:12 +0000

    added relationship

--- src/app/components/home/home-controller.js
+++ src/app/components/home/home-controller.js
@@ -1,27 +1,31 @@
 (function() {
     'use strict';
-    angular.module('application').controller('HomeController', function($rootScope, $scope, $log, $q, $state, $stateParams, HomeServices) {
+    angular.module('application').controller('HomeController', function($rootScope, $window, $scope, $log, $q, $state, $stateParams, HomeServices) {
         self = this;
         $rootScope.pageTitle = 'Home';
 
         self.recipes = {};
         self.recipe = {};
 
+        self.backButton = function() {
+          console.log('this works!');
+         $window.history.back();
+        }
+
 
         switch ($state.current.name) {
 
-            case 'home':
+            case 'homepage':
 
                 HomeServices.getAllRecipes().then(
                     function(result) {
-                        // console.log(result);
+                        console.log(result);
                         self.recipes = result.data;
                     },
                     function(err) {
                         console.log('Error saving to endpoint: ', err);
                     });
                 break;
-
 
 
             case 'recipe':
@@ -36,13 +40,8 @@
                     });
                 break;
 
-
-
             default:
         }
-
-
-
 
     });
 })();
